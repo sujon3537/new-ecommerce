ab4af6be8bd19bcac9d7aa7b418f1497a8787207
sujon3537 <119605414+sujon3537@users.noreply.github.com>
2023-09-12 23:48:38 +0600

    homepage update

--- src/components/TopCategories.js
+++ src/components/TopCategories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const TopCategories = () => {
   return (
@@ -8,98 +8,190 @@ const TopCategories = () => {
       </div>
       <div className="flex justify-between flex-wrap gap-4 mt-8">
         <div className="w-[32%] flex gap-x-3 items-center border border-[#ccc] px-3">
-            <div className="w-1/2">
-                <img src="./assets/images/watch.jpg" alt="watch" className="w-full"/>
-            </div>
-            <div>
-                <h2 className="font-work text-base text-black hover:text-primary mb-3"><a href="#">Electronics</a></h2>
-                <ul className="font-work text-sm text-textColor gap-2">
-                    <li className=""><a href="">TV Televisions</a></li>
-                    <li className=""><a href="">Air Conditioners</a></li>
-                    <li className=""><a href="">Washing Machines</a></li>
-                    <li className=""><a href="">Audio & Theaters</a></li>
-                    <li className=""><a href="">Office Electronics</a></li>
-                </ul>
-            </div>
+          <div className="w-1/2">
+            <img
+              src="./assets/images/watch.jpg"
+              alt="watch"
+              className="w-full"
+            />
+          </div>
+          <div>
+            <h2 className="font-work text-base text-black hover:text-primary mb-3">
+              <a href="#">Electronics</a>
+            </h2>
+            <ul className="font-work text-sm text-textColor gap-2">
+              <li className="">
+                <a href="">TV Televisions</a>
+              </li>
+              <li className="">
+                <a href="">Air Conditioners</a>
+              </li>
+              <li className="">
+                <a href="">Washing Machines</a>
+              </li>
+              <li className="">
+                <a href="">Audio & Theaters</a>
+              </li>
+              <li className="">
+                <a href="">Office Electronics</a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="w-[32%] flex gap-x-3 items-center border border-[#ccc] px-3">
-            <div className="w-1/2">
-                <img src="./assets/images/bag.jpg" alt="watch" className="w-full"/>
-            </div>
-            <div>
-                <h2 className="font-work text-base text-black hover:text-primary mb-3"><a href="#">Electronics</a></h2>
-                <ul className="font-work text-sm text-textColor gap-2">
-                    <li className=""><a href="">TV Televisions</a></li>
-                    <li className=""><a href="">Air Conditioners</a></li>
-                    <li className=""><a href="">Washing Machines</a></li>
-                    <li className=""><a href="">Audio & Theaters</a></li>
-                    <li className=""><a href="">Office Electronics</a></li>
-                </ul>
-            </div>
+          <div className="w-1/2">
+            <img src="./assets/images/bag.jpg" alt="watch" className="w-full" />
+          </div>
+          <div>
+            <h2 className="font-work text-base text-black hover:text-primary mb-3">
+              <a href="#">Traveling</a>
+            </h2>
+            <ul className="font-work text-sm text-textColor gap-2">
+              <li className="">
+                <a href="">TV Televisions</a>
+              </li>
+              <li className="">
+                <a href="">Air Conditioners</a>
+              </li>
+              <li className="">
+                <a href="">Washing Machines</a>
+              </li>
+              <li className="">
+                <a href="">Audio & Theaters</a>
+              </li>
+              <li className="">
+                <a href="">Office Electronics</a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="w-[32%] flex gap-x-3 items-center border border-[#ccc] px-3">
-            <div className="w-1/2">
-                <img src="./assets/images/shirt.jpg" alt="watch" className="w-full"/>
-            </div>
-            <div>
-                <h2 className="font-work text-base text-black hover:text-primary mb-3"><a href="#">Electronics</a></h2>
-                <ul className="font-work text-sm text-textColor gap-2">
-                    <li className=""><a href="">TV Televisions</a></li>
-                    <li className=""><a href="">Air Conditioners</a></li>
-                    <li className=""><a href="">Washing Machines</a></li>
-                    <li className=""><a href="">Audio & Theaters</a></li>
-                    <li className=""><a href="">Office Electronics</a></li>
-                </ul>
-            </div>
+          <div className="w-1/2">
+            <img
+              src="./assets/images/shirt.jpg"
+              alt="watch"
+              className="w-full"
+            />
+          </div>
+          <div>
+            <h2 className="font-work text-base text-black hover:text-primary mb-3">
+              <a href="#">Clothing</a>
+            </h2>
+            <ul className="font-work text-sm text-textColor gap-2">
+              <li className="">
+                <a href="">TV Televisions</a>
+              </li>
+              <li className="">
+                <a href="">Air Conditioners</a>
+              </li>
+              <li className="">
+                <a href="">Washing Machines</a>
+              </li>
+              <li className="">
+                <a href="">Audio & Theaters</a>
+              </li>
+              <li className="">
+                <a href="">Office Electronics</a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="w-[32%] flex gap-x-3 items-center border border-[#ccc] px-3">
-            <div className="w-1/2">
-                <img src="./assets/images/backpack.jpg" alt="watch" className="w-full"/>
-            </div>
-            <div>
-                <h2 className="font-work text-base text-black hover:text-primary mb-3"><a href="#">Electronics</a></h2>
-                <ul className="font-work text-sm text-textColor gap-2">
-                    <li className=""><a href="">TV Televisions</a></li>
-                    <li className=""><a href="">Air Conditioners</a></li>
-                    <li className=""><a href="">Washing Machines</a></li>
-                    <li className=""><a href="">Audio & Theaters</a></li>
-                    <li className=""><a href="">Office Electronics</a></li>
-                </ul>
-            </div>
+          <div className="w-1/2">
+            <img
+              src="./assets/images/backpack.jpg"
+              alt="watch"
+              className="w-full"
+            />
+          </div>
+          <div>
+            <h2 className="font-work text-base text-black hover:text-primary mb-3">
+              <a href="#">Bags</a>
+            </h2>
+            <ul className="font-work text-sm text-textColor gap-2">
+              <li className="">
+                <a href="">TV Televisions</a>
+              </li>
+              <li className="">
+                <a href="">Air Conditioners</a>
+              </li>
+              <li className="">
+                <a href="">Washing Machines</a>
+              </li>
+              <li className="">
+                <a href="">Audio & Theaters</a>
+              </li>
+              <li className="">
+                <a href="">Office Electronics</a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="w-[32%] flex gap-x-3 items-center border border-[#ccc] px-3">
-            <div className="w-1/2">
-                <img src="./assets/images/gamebox.jpg" alt="watch" className="w-full"/>
-            </div>
-            <div>
-                <h2 className="font-work text-base text-black hover:text-primary mb-3"><a href="#">Electronics</a></h2>
-                <ul className="font-work text-sm text-textColor gap-2">
-                    <li className=""><a href="">TV Televisions</a></li>
-                    <li className=""><a href="">Air Conditioners</a></li>
-                    <li className=""><a href="">Washing Machines</a></li>
-                    <li className=""><a href="">Audio & Theaters</a></li>
-                    <li className=""><a href="">Office Electronics</a></li>
-                </ul>
-            </div>
+          <div className="w-1/2">
+            <img
+              src="./assets/images/gamebox.jpg"
+              alt="watch"
+              className="w-full"
+            />
+          </div>
+          <div>
+            <h2 className="font-work text-base text-black hover:text-primary mb-3">
+              <a href="#">Accessories</a>
+            </h2>
+            <ul className="font-work text-sm text-textColor gap-2">
+              <li className="">
+                <a href="">TV Televisions</a>
+              </li>
+              <li className="">
+                <a href="">Air Conditioners</a>
+              </li>
+              <li className="">
+                <a href="">Washing Machines</a>
+              </li>
+              <li className="">
+                <a href="">Audio & Theaters</a>
+              </li>
+              <li className="">
+                <a href="">Office Electronics</a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="w-[32%] flex gap-x-3 items-center border border-[#ccc] px-3">
-            <div className="w-1/2">
-                <img src="./assets/images/watch.jpg" alt="watch" className="w-full"/>
-            </div>
-            <div>
-                <h2 className="font-work text-base text-black hover:text-primary mb-3"><a href="#">Electronics</a></h2>
-                <ul className="font-work text-sm text-textColor gap-2">
-                    <li className=""><a href="">TV Televisions</a></li>
-                    <li className=""><a href="">Air Conditioners</a></li>
-                    <li className=""><a href="">Washing Machines</a></li>
-                    <li className=""><a href="">Audio & Theaters</a></li>
-                    <li className=""><a href="">Office Electronics</a></li>
-                </ul>
-            </div>
+          <div className="w-1/2">
+            <img
+              src="./assets/images/watch.jpg"
+              alt="watch"
+              className="w-full"
+            />
+          </div>
+          <div>
+            <h2 className="font-work text-base text-black hover:text-primary mb-3">
+              <a href="#">Others</a>
+            </h2>
+            <ul className="font-work text-sm text-textColor gap-2">
+              <li className="">
+                <a href="">TV Televisions</a>
+              </li>
+              <li className="">
+                <a href="">Air Conditioners</a>
+              </li>
+              <li className="">
+                <a href="">Washing Machines</a>
+              </li>
+              <li className="">
+                <a href="">Audio & Theaters</a>
+              </li>
+              <li className="">
+                <a href="">Office Electronics</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopCategories
+export default TopCategories;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import Banner from '../components/Banner'
-import CategorySlides from '../components/CategorySlides'
-import TopCategories from '../components/TopCategories'
-import Deals from '../components/Deals'
+import React from "react";
+import Banner from "../components/Banner";
+import CategorySlides from "../components/CategorySlides";
+import TopCategories from "../components/TopCategories";
+import Deals from "../components/Deals";
 
 const Home = () => {
   return (
     <>
-        <Banner/>
-        <Deals/>
-        <TopCategories/>
-        <CategorySlides/>
+      <Banner />
+      <TopCategories />
+      <CategorySlides />
+      <Deals heading="Electronic Items" />
+      <Deals heading="Clothing Items" />
+      <Deals heading="Computer Items" />
+      <Deals heading="Farniture Items" />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
